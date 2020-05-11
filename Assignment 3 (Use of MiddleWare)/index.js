e10bbce2ef9535de4492c245c259e7df8a7dda55
aherrahul1995 <43370315+aherrahul1995@users.noreply.github.com>
2020-05-11 12:47:15 +0530

    Middleware test completed

--- Assignment 3 (Use of MiddleWare)/index.js
+++ Assignment 3 (Use of MiddleWare)/index.js
@@ -8,6 +8,25 @@ app.get('/', (req, res) => {
     res.send("Hello World..!!");
 });
 
+const admin = (req, res) => {
+    return res.send("I am Admin..!!");
+};
+
+// flag to restrict middleware
+const checkAdmin = true;
+
+const isAdmin = (req, res, next) => {
+    if (!checkAdmin) {
+        console.log("Your not Admin");
+    } else {
+        console.log("isAdmin is running..!!");
+        next();
+    }
+}
+
+// Test Of Middleware
+app.get('/admin', isAdmin, admin);
+
 app.get('/login', (req, res) => {
     res.send("Login Works..!!");
 });
